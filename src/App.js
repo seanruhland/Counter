a352a69fa19de2b6c73d27bc06f7790eaaf6d1e4
Sean Ruhland <sean.ruhland@gmail.com>
2020-08-08 09:32:39 -0700

    button increments and count

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import './App.css';
 import Button from './components/Button'
 
 function App() {
+  let count = 0;
+
   return (
     <div className="App">
       <Button increment={1}/>
+      <Button increment={10}/>
+      <Button increment={100}/>
+      <Button increment={1000}/>
+      <span>{count}</span>
     </div>
   );
 }
